85c7557e3b72a09b897996e9e3ce3d0dafe1a026
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-11-04 14:31:48 -0700

    constructor challenge

--- Java_Master_Course/Section6/src/ChaseBank.java
+++ Java_Master_Course/Section6/src/ChaseBank.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 public class ChaseBank {
     private String customerName;
     private int accountNumber;
-    private int balance;
+    private double balance;
     private String email;
     private String phoneNumber;
 
     public ChaseBank(){
-        this("Default firstName,", "Default LastName", 000_000_000,
-                2,"No email", "000-000-0000" );
+        this("John Doe", 000_000_000,
+                2.0,"No email", "000-000-0000" );
         System.out.println("Empty constructor called");
     }
-    public ChaseBank(String firstName, String lastName, int accountNumber, int balance, String email, String phoneNumber){
-        setCustomerName(firstName, lastName);
-        setAccountNumber(accountNumber);
-        setBalance(balance);
-        setEmail(email);
-        setPhoneNumber(phoneNumber);
+    public ChaseBank(String customerName, int accountNumber, double balance, String email, String phoneNumber){
+//        setCustomerName(firstName, lastName);
+//        setAccountNumber(accountNumber);
+//        setBalance(balance);
+//        setEmail(email);
+//        setPhoneNumber(phoneNumber);
+        this.customerName = customerName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
         System.out.println("Account constructor with parameters called");
+    }
+
+    public ChaseBank(String customerName, String email, String phoneNumber) {
+        this(customerName, 999999, 100.50, email, phoneNumber);
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
     private void setCustomerName(String firstName, String lastName){
@@ -40,11 +52,11 @@ public class ChaseBank {
         return this.accountNumber;
     }
 
-    private void setBalance(int balance){
+    private void setBalance(double balance){
         this.balance = balance;
     }
 
-    public int getBalance(){
+    public double getBalance(){
         return this.balance;
     }
 
@@ -60,16 +72,16 @@ public class ChaseBank {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getPhoneNumber(String phoneNumber) {
+    public String getPhoneNumber() {
         return this.phoneNumber;
     }
 
-    public void depositFunds(int deposit){
+    public void depositFunds(double deposit){
         setBalance(getBalance() + deposit);
         System.out.println("New balance is: " + this.getBalance());
     }
 
-    public void withdraw(int withdraw){
+    public void withdraw(double withdraw){
         if(getBalance() - withdraw < 0){
             System.out.println("Insufficient funds");
             System.out.println("Your current balance is: " + getBalance());
@@ -87,28 +99,34 @@ public class ChaseBank {
 //        ChaseBank bank = new ChaseBank("Chase", "Whitney", 556_221_867, 0,
 //                "cjwhitney2892@gmail.com", "360-250-4332");
 
-        System.out.println("Welcome to Chase Bank");
+//        System.out.println("Welcome to Chase Bank");
 
-        while (true) {
+//        while (true) {
+//
+//            System.out.println("1) View balance " +
+//                    "2) Make a deposit " +
+//                    "3) Make a withdraw");
+//            int input = user.nextInt();
+//
+//            if (input == 1) {
+//                System.out.println("Balance: " + bank.getBalance());
+//            } else if (input == 2) {
+//                System.out.println("Please enter an amount you want to deposit");
+//                input = user.nextInt();
+//                bank.depositFunds(input);
+//            } else if (input == 3) {
+//                System.out.println("Please enter the amount you want to withdraw");
+//                input = user.nextInt();
+//                bank.withdraw(input);
+//
+//            }
+//        }
+//
+//        ChaseBank chaseAccount = new ChaseBank("Chase Whitney", "chase@gmail.com",
+//                "123456789");
+//
+//        System.out.println(chaseAccount.getPhoneNumber() + " name " + chaseAccount.getCustomerName());
 
-            System.out.println("1) View balance " +
-                    "2) Make a deposit " +
-                    "3) Make a withdraw");
-            int input = user.nextInt();
-
-            if (input == 1) {
-                System.out.println("Balance: " + bank.getBalance());
-            } else if (input == 2) {
-                System.out.println("Please enter an amount you want to deposit");
-                input = user.nextInt();
-                bank.depositFunds(input);
-            } else if (input == 3) {
-                System.out.println("Please enter the amount you want to withdraw");
-                input = user.nextInt();
-                bank.withdraw(input);
-
-            }
-        }
     }
 
 }
---
+++ Java_Master_Course/Section6/src/VipCustomer.java
@@ -0,0 +1,60 @@
+public class VipCustomer {
+
+    private String name;
+    private int creditLimit;
+    private String email;
+
+
+
+    public VipCustomer(){
+        this("Unknown", "XXX@XXXXX.com", 0);
+    }
+
+    public VipCustomer(String name, String email){
+        this(name, email,1000);
+
+    }
+
+    public VipCustomer(String name, String email, int creditLimit){
+        this.name = name;
+        this.email = email;
+        this.creditLimit = creditLimit;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCreditLimit() {
+        return this.creditLimit;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+
+
+    public static void main(String[] args) {
+        VipCustomer chaseWhitneyACC = new VipCustomer("Chase whitney", "cjwhtiney2892@gmail.com", 25000);
+
+        System.out.println("Name: " + chaseWhitneyACC.getName());
+        System.out.println("Email: " + chaseWhitneyACC.getEmail());
+        System.out.println("Credit Limit: $" + chaseWhitneyACC.getCreditLimit());
+        System.out.println("_________________________________");
+
+        VipCustomer newCust1 = new VipCustomer();
+
+        System.out.println("Name: " + newCust1.getName());
+        System.out.println("Email: " + newCust1.getEmail());
+        System.out.println("Credit Limit: $" + newCust1.getCreditLimit());
+        System.out.println("_________________________________");
+
+        VipCustomer newCust2 = new VipCustomer("Tim Burchka", "Tim@Gmail.com");
+
+        System.out.println("Name: " + newCust2.getName());
+        System.out.println("Email: " + newCust2.getEmail());
+        System.out.println("Credit Limit: $" + newCust2.getCreditLimit());
+        System.out.println("_________________________________");
+    }
+}
