7bec61bf01773c9be23ddc441dacaa36914cef9c
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-11-03 19:46:05 -0700

    bank class challenge

--- Java_Master_Course/Section6/src/ChaseBank.java
+++ Java_Master_Course/Section6/src/ChaseBank.java
@@ -27,7 +27,7 @@ public class ChaseBank {
     }
 
     public void setBalance(int balance){
-        this.balance = this.balance + balance;
+        this.balance = balance;
     }
 
     public int getBalance(){
@@ -51,18 +51,46 @@ public class ChaseBank {
     }
 
     public void depositFunds(int deposit){
-        setBalance(deposit);
+        setBalance(getBalance() + deposit);
         System.out.println("New balance is: " + this.getBalance());
     }
 
     public void withdraw(int withdraw){
-        if(this.getBalance() - withdraw < 0){
+        if(getBalance() - withdraw < 0){
             System.out.println("Insufficient funds");
-            System.out.println("Your current balance is: " + this.getBalance());
+            System.out.println("Your current balance is: " + getBalance());
         } else {
-            System.out.println("Current balance is: " + this.getBalance());
-            setBalance(this.getBalance()-withdraw);
-            System.out.println("New balance is: " + this.getBalance());
+            System.out.println("Current balance is: " + getBalance());
+            setBalance(getBalance() - withdraw);
+            System.out.println("New balance is: " + getBalance());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        ChaseBank bank = new ChaseBank();
+
+        System.out.println("Welcome to Chase Bank");
+
+        while (true) {
+
+            System.out.println("1) View balance " +
+                    "2) Make a deposit " +
+                    "3) Make a withdraw");
+            int input = user.nextInt();
+
+            if (input == 1) {
+                System.out.println("Balance: " + bank.getBalance());
+            } else if (input == 2) {
+                System.out.println("Please enter an amount you want to deposit");
+                input = user.nextInt();
+                bank.depositFunds(input);
+            } else if (input == 3) {
+                System.out.println("Please enter the amount you want to withdraw");
+                input = user.nextInt();
+                bank.withdraw(input);
+
+            }
         }
     }
 
